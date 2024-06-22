785af59119113957d16716b7939f32e36fc4cba7
andrex903 <50050676+andrex903@users.noreply.github.com>
2024-06-22 14:58:05 +0200

    Update SnapToGround.cs

--- Editor/Overlays/SnapToGround.cs
+++ Editor/Overlays/SnapToGround.cs
@@ -11,8 +11,20 @@ namespace RedeevEditor.Utilities
     public class SnapToGround : IMGUIOverlay
     {
         private int mask = ~0;
+        private Direction direction = Direction.Down;
         private float offset = 0f;
         public const string overlayID = "SnapToGround";
+
+        private enum Direction
+        {
+            All,
+            Down,
+            Up,
+            Right,
+            Left,
+            Forward,
+            Backward
+        }
 
         public override void OnGUI()
         {
@@ -20,36 +32,90 @@ namespace RedeevEditor.Utilities
             LayerMask tempMask = EditorGUILayout.MaskField(InternalEditorUtility.LayerMaskToConcatenatedLayersMask(mask), InternalEditorUtility.layers);
             mask = InternalEditorUtility.ConcatenatedLayersMaskToLayerMask(tempMask);
 
+            direction = (Direction)EditorGUILayout.EnumPopup(direction);
+
             var selection = Selection.transforms;
             int count = selection.Length;
             if (count == 0) GUI.enabled = false;
             string text = count == 1 ? $"Snap ({selection[0].name})" : $"Snap ({count} elements)";
-            if (GUILayout.Button(text)) SetPostion(selection, Vector3.up);
+            if (GUILayout.Button(text, GUILayout.Height(25f))) SetPostion(selection);
             GUI.enabled = true;
         }
 
-        private void SetPostion(Transform[] selection, Vector3 direction)
+        private void SetPostion(Transform[] selection)
         {
             Undo.SetCurrentGroupName("GameObjects snapped");
             int group = Undo.GetCurrentGroup();
 
             Undo.RecordObjects(selection, "transform selected objects");
 
-            for (int i = 0; i < selection.Length; i++)
+            foreach (var selected in selection)
             {
-                var selected = selection[i];   
-                RaycastHit[] hits = Physics.RaycastAll(selected.position + direction * 500f, -direction, 1000f);
-                for (int j = 0; j < hits.Length; j++)
-                {
-                    if (hits[j].transform == selected) continue;
-                    if (mask != (mask | (1 << hits[j].transform.gameObject.layer))) continue;
-
-                    selected.position = hits[j].point + Vector3.up * offset;
-                    break;
-                }
+                SetPosition(selected);
             }
 
             Undo.CollapseUndoOperations(group);
+        }
+
+        private void SetPosition(Transform selected)
+        {
+            float distance = float.MaxValue;
+            Vector3 hitPoint = selected.position;
+            Vector3 resultDirection = Vector3.zero;
+
+            switch (direction)
+            {
+                case Direction.Down:
+                    TestDirection(Vector3.down, false);
+                    break;
+                case Direction.Up:
+                    TestDirection(Vector3.up, false);
+                    break;
+                case Direction.Right:
+                    TestDirection(Vector3.right, false);
+                    break;
+                case Direction.Left:
+                    TestDirection(Vector3.left, false);
+                    break;
+                case Direction.Forward:
+                    TestDirection(Vector3.forward, false);
+                    break;
+                case Direction.Backward:
+                    TestDirection(Vector3.back, false);
+                    break;
+                case Direction.All:
+                    TestDirection(Vector3.forward);
+                    TestDirection(Vector3.back);
+                    TestDirection(Vector3.left);
+                    TestDirection(Vector3.right);
+                    TestDirection(Vector3.up);
+                    TestDirection(Vector3.down);
+                    break;
+            }
+
+            selected.position = hitPoint + resultDirection * offset;
+
+            void TestDirection(Vector3 direction, bool checkDistance = true)
+            {
+                if (Physics.Raycast(selected.position, direction, out RaycastHit hit, 1000f, mask))
+                {
+                    if (checkDistance)
+                    {
+                        float currentDistance = Vector3.Distance(selected.position, hit.point);
+                        if (currentDistance < distance)
+                        {
+                            distance = currentDistance;
+                            hitPoint = hit.point;
+                            resultDirection = -direction;
+                        }
+                    }
+                    else
+                    {
+                        hitPoint = hit.point;
+                        resultDirection = -direction;
+                    }
+                }
+            }
         }
     }
 }
