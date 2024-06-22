ad43f2048588b8495022b579c224887b47cfc8ae
andrex903 <50050676+andrex903@users.noreply.github.com>
2024-06-22 11:26:46 +0200

    update scene toobar

--- Editor/Toolbar/ScenesToolbar.cs
+++ Editor/Toolbar/ScenesToolbar.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace RedeevEditor.Utilities
 {
     [InitializeOnLoad]
     public static class ScenesToolbar
     {
-        private static int currentPath = 0;
-        private static int lastPath = 0;
+        private static int activeSceneIndex = 0;
+        private static int lastSceneIndex = -1;
+
         private readonly static List<string> scenes;
         private readonly static List<string> sceneNames;
 
@@ -18,33 +20,60 @@ namespace RedeevEditor.Utilities
         {
             ToolbarExtension.LeftToolbarGUI.Add(OnToolbarGUI);
 
+            EditorSceneManager.activeSceneChangedInEditMode += OnSceneChanged;
+
             scenes = new();
             sceneNames = new();
-            foreach (var scene in EditorBuildSettings.scenes)
-            {
-                scenes.Add(scene.path);
-                sceneNames.Add(System.IO.Path.GetFileNameWithoutExtension(scene.path));
-            }
+            FindAllScenes();
+        }
 
-            currentPath = scenes.IndexOf(EditorSceneManager.GetActiveScene().path);
-            lastPath = currentPath;            
+        private static void OnSceneChanged(Scene _, Scene scene)
+        {
+            int index = scenes.IndexOf(scene.path);
+            if (index >= 0 && index < scenes.Count)
+            {
+                activeSceneIndex = index;
+            }
         }
 
         private static void OnToolbarGUI()
         {
             if (EditorApplication.isPlaying) GUI.enabled = false;
             GUILayout.Space(5);
-            currentPath = EditorGUILayout.Popup(currentPath, sceneNames.ToArray(), EditorStyles.toolbarPopup, GUILayout.Width(100f));
-            if (currentPath != lastPath)
+
+            activeSceneIndex = EditorGUILayout.Popup(activeSceneIndex, sceneNames.ToArray(), EditorStyles.toolbarPopup, GUILayout.Width(150f));
+            if (activeSceneIndex != lastSceneIndex)
             {
                 if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
                 {
-                    lastPath = currentPath;
-                    EditorSceneManager.OpenScene(scenes[currentPath]);
+                    lastSceneIndex = activeSceneIndex;
+                    EditorSceneManager.OpenScene(scenes[activeSceneIndex]);
                 }
-                else currentPath = lastPath;
+                else activeSceneIndex = lastSceneIndex;
             }
+
+            GUILayout.Space(2);
+
+            if (EditorUtilityGUI.IconButton("d_Refresh@2x", 30f, EditorStyles.toolbarButton, "Refresh Scenes"))
+            {
+                FindAllScenes();
+            }
+
             GUI.enabled = true;
+        }
+
+        private static void FindAllScenes()
+        {
+            scenes.Clear();
+            sceneNames.Clear();
+
+            string[] guids = AssetDatabase.FindAssets("t:Scene", new string[] { "Assets/Scenes" });
+            foreach (var guid in guids)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                scenes.Add(path);
+                sceneNames.Add(System.IO.Path.GetFileNameWithoutExtension(path));
+            }
         }
     }
 }
--- Editor/Toolbar/ToolbarExtension.cs
+++ Editor/Toolbar/ToolbarExtension.cs
@@ -11,125 +11,31 @@ namespace RedeevEditor.Utilities
     [InitializeOnLoad]
     public static class ToolbarExtension
     {
-        public const float space = 8;
-        public const float largeSpace = 20;
-        public const float buttonWidth = 32;
-        public const float dropdownWidth = 80;
-        public const float playPauseStopWidth = 140;
-
-        private static readonly int m_toolCount;
-        private static GUIStyle m_commandStyle = null;
-
         public static readonly List<Action> LeftToolbarGUI = new();
         public static readonly List<Action> RightToolbarGUI = new();
 
         static ToolbarExtension()
         {
-            Type toolbarType = typeof(Editor).Assembly.GetType("UnityEditor.Toolbar");
-
-            string fieldName = "k_ToolCount";
-
-            FieldInfo toolIcons = toolbarType.GetField(fieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
-
-            m_toolCount = toolIcons != null ? ((int)toolIcons.GetValue(null)) : 8;
-
-            ToolbarCallback.OnToolbarGUI = OnGUI;
-            ToolbarCallback.OnToolbarGUILeft = GUILeft;
-            ToolbarCallback.OnToolbarGUIRight = GUIRight;
+            ToolbarCallback.OnToolbarGUILeft = OnToolbarGUILeft;
+            ToolbarCallback.OnToolbarGUIRight = OnToolbarGUIRight;
         }
 
-        private static void OnGUI()
+        public static void OnToolbarGUILeft()
         {
-            // Create two containers, left and right
-            // Screen is whole toolbar
-
-            if (m_commandStyle == null)
-            {
-                m_commandStyle = new GUIStyle("CommandLeft");
-            }
-
-            var screenWidth = EditorGUIUtility.currentViewWidth;
-
-            // Following calculations match code reflected from Toolbar.OldOnGUI()
-            float playButtonsPosition = Mathf.RoundToInt((screenWidth - playPauseStopWidth) / 2);
-
-            Rect leftRect = new(0, 0, screenWidth, Screen.height);
-            leftRect.xMin += space; // Spacing left
-            leftRect.xMin += buttonWidth * m_toolCount; // Tool buttons
-
-            leftRect.xMin += space; // Spacing between tools and pivot
-
-            leftRect.xMin += 64 * 2; // Pivot buttons
-            leftRect.xMax = playButtonsPosition;
-
-            Rect rightRect = new(0, 0, screenWidth, Screen.height);
-            rightRect.xMin = playButtonsPosition;
-            rightRect.xMin += m_commandStyle.fixedWidth * 3; // Play buttons
-            rightRect.xMax = screenWidth;
-            rightRect.xMax -= space; // Spacing right
-            rightRect.xMax -= dropdownWidth; // Layout
-            rightRect.xMax -= space; // Spacing between layout and layers
-            rightRect.xMax -= dropdownWidth; // Layers
-            rightRect.xMax -= space; // Spacing between layers and account
-            rightRect.xMax -= dropdownWidth; // Account
-            rightRect.xMax -= space; // Spacing between account and cloud
-            rightRect.xMax -= buttonWidth; // Cloud
-            rightRect.xMax -= space; // Spacing between cloud and collab
-            rightRect.xMax -= 78; // Colab
-            // Add spacing around existing controls
-            leftRect.xMin += space;
-            leftRect.xMax -= space;
-            rightRect.xMin += space;
-            rightRect.xMax -= space;
-            // Add top and bottom margins
-            leftRect.y = 4;
-            leftRect.height = 22;
-            rightRect.y = 4;
-            rightRect.height = 22;
-
-            if (leftRect.width > 0)
-            {
-                GUILayout.BeginArea(leftRect);
-                GUILayout.BeginHorizontal();
-                foreach (var handler in LeftToolbarGUI)
-                {
-                    handler();
-                }
-
-                GUILayout.EndHorizontal();
-                GUILayout.EndArea();
-            }
-
-            if (rightRect.width > 0)
-            {
-                GUILayout.BeginArea(rightRect);
-                GUILayout.BeginHorizontal();
-                foreach (var handler in RightToolbarGUI)
-                {
-                    handler();
-                }
-
-                GUILayout.EndHorizontal();
-                GUILayout.EndArea();
-            }
+            OnToolbarGUI(LeftToolbarGUI);
         }
 
-        public static void GUILeft()
+        public static void OnToolbarGUIRight()
+        {
+            OnToolbarGUI(RightToolbarGUI);
+        }
+
+        private static void OnToolbarGUI(List<Action> handlers)
         {
             GUILayout.BeginHorizontal();
-            foreach (var handler in LeftToolbarGUI)
+            foreach (var handler in handlers)
             {
-                handler();
-            }
-            GUILayout.EndHorizontal();
-        }
-
-        public static void GUIRight()
-        {
-            GUILayout.BeginHorizontal();
-            foreach (var handler in RightToolbarGUI)
-            {
-                handler();
+                handler?.Invoke();
             }
             GUILayout.EndHorizontal();
         }
@@ -138,12 +44,8 @@ namespace RedeevEditor.Utilities
     public static class ToolbarCallback
     {
         private readonly static Type m_toolbarType = typeof(Editor).Assembly.GetType("UnityEditor.Toolbar");
-        private static ScriptableObject m_currentToolbar;
+        private static ScriptableObject currentToolbar;
 
-        /// <summary>
-        /// Callback for toolbar OnGUI method.
-        /// </summary>
-        public static Action OnToolbarGUI;
         public static Action OnToolbarGUILeft;
         public static Action OnToolbarGUIRight;
 
@@ -156,39 +58,32 @@ namespace RedeevEditor.Utilities
         private static void OnUpdate()
         {
             // Relying on the fact that toolbar is ScriptableObject and gets deleted when layout changes
-            if (m_currentToolbar == null)
+            if (currentToolbar != null) return; 
+
+            var toolbars = Resources.FindObjectsOfTypeAll(m_toolbarType);
+
+            currentToolbar = toolbars.Length > 0 ? (ScriptableObject)toolbars[0] : null;
+            if (currentToolbar != null)
             {
-                // Find toolbar
-                var toolbars = Resources.FindObjectsOfTypeAll(m_toolbarType);
-                m_currentToolbar = toolbars.Length > 0 ? (ScriptableObject)toolbars[0] : null;
-                if (m_currentToolbar != null)
+                var root = currentToolbar.GetType().GetField("m_Root", BindingFlags.NonPublic | BindingFlags.Instance);
+                var rawRoot = root.GetValue(currentToolbar);
+                var mRoot = rawRoot as VisualElement;
+                RegisterCallback("ToolbarZoneLeftAlign", OnToolbarGUILeft);
+                RegisterCallback("ToolbarZoneRightAlign", OnToolbarGUIRight);
+
+                void RegisterCallback(string root, Action action)
                 {
-                    var root = m_currentToolbar.GetType().GetField("m_Root", BindingFlags.NonPublic | BindingFlags.Instance);
-                    var rawRoot = root.GetValue(m_currentToolbar);
-                    var mRoot = rawRoot as VisualElement;
-                    RegisterCallback("ToolbarZoneLeftAlign", OnToolbarGUILeft);
-                    RegisterCallback("ToolbarZoneRightAlign", OnToolbarGUIRight);
+                    var toolbarZone = mRoot.Q(root);
 
-                    void RegisterCallback(string root, Action cb)
+                    var parent = new VisualElement()
                     {
-                        var toolbarZone = mRoot.Q(root);
-
-                        var parent = new VisualElement()
-                        {
-                            style = {
-                                flexGrow = 1,
-                                flexDirection = FlexDirection.Row,
-                            }
-                        };
-                        var container = new IMGUIContainer();
-                        container.style.flexGrow = 1;
-                        container.onGUIHandler += () =>
-                        {
-                            cb?.Invoke();
-                        };
-                        parent.Add(container);
-                        toolbarZone.Add(parent);
-                    }
+                        style = { flexGrow = 1, flexDirection = FlexDirection.Row, }
+                    };
+                    var container = new IMGUIContainer();
+                    container.style.flexGrow = 1;
+                    container.onGUIHandler += action;
+                    parent.Add(container);
+                    toolbarZone.Add(parent);
                 }
             }
         }
